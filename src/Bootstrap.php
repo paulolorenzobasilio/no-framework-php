648e02d4dfe35ed8b2fe5c206f9d969f1437876d
Paulo Basilio <paulo@pop-os.localdomain>
2020-02-19 13:58:19 +0800

    http request&response

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -23,4 +23,11 @@ if($environment !== 'production'){
 }
 $whoops->register();
 
-throw new \Exception;
+$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new \Http\HttpResponse;
+
+foreach($response->getHeaders() as $header){
+    header($header, false);
+}
+
+echo $response->getContent();
