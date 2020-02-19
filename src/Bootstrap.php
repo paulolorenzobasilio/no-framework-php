77c05e89f8b6da08a5fa29cace5a3e836775bac9
Paulo Basilio <paulo@pop-os.localdomain>
2020-02-19 14:48:55 +0800

    auryn: dependency injector

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -20,8 +20,10 @@ if ($environment !== 'production') {
 $whoops->register();
 
 // 04 - http
-$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
-$response = new \Http\HttpResponse;
+$injector = include('Dependencies.php');
+
+$request = $injector->make('Http\HttpRequest');
+$response = $injector->make('Http\HttpResponse');
 
 foreach ($response->getHeaders() as $header) {
     header($header, false);
@@ -59,7 +61,7 @@ switch ($routeInfo[0]) {
         $method = $routeInfo[1][1];
         $vars = $routeInfo[2];
 
-        $class = new $className($response);
+        $class = $injector->make($className);
         $class->$method($vars);
         break;
 }
--- src/Controllers/Homepage.php
+++ src/Controllers/Homepage.php
@@ -2,19 +2,24 @@
 
 namespace App\Controllers;
 
+use Http\Request;
 use Http\Response;
 
 class Homepage
 {
     private $response;
+    private $request;
 
-    public function __construct(Response $response)
+    public function __construct(Response $response, Request $request)
     {
         $this->response = $response;
+        $this->request = $request;
     }
 
     public function show()
     {
-        $this->response->setContent('Hello World');
+        $content = '<h1>Hello World</h1>';
+        $content.= 'Hello ' . $this->request->getParameter('name', 'stranger');
+        $this->response->setContent($content);
     }
 }
