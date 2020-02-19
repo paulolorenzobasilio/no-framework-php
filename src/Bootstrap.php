874f879c094ec15cbfdc5a68cabc7cc4e67bf05d
Paulo Basilio <paulo@pop-os.localdomain>
2020-02-19 13:44:43 +0800

    error-handler

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,26 @@
-<?php declare(strict_types=1);
+<?php
 
-echo 'Hello World';
+namespace App;
+
+use Exception;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+$environment = 'development';
+
+/**
+ * Register the error handler
+ */
+$whoops = new \Whoops\Run;
+if($environment !== 'production'){
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+    $whoops->pushHandler(function ($e){
+        echo 'Todo: friendly error page and send an email to the developer';
+    });
+}
+$whoops->register();
+
+throw new \Exception;
